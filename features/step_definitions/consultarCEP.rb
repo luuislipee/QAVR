fe1ab796122b7fde4df25bda58617c5f25460d56
Luis Felipe Chiaradia <macos@MacBook-Luis.local>
2020-07-21 17:23:31 -0300

    Atualizando o arquivo consultarCEP.rb

--- features/step_definitions/consultarCEP.rb
+++ features/step_definitions/consultarCEP.rb
@@ -1,15 +1,14 @@
 Given("uma base de dados") do
     $uri_base = "https://viacep.com.br/ws/01001000/json/"
-    log "Insira um CEP para busca"
 end
 
 When("realizar uma busca por um cep") do
-    $cep = gets.chomp
     $response = HTTParty.get($uri_base)
+    log "Insira um CEP para busca"
 end
 
 Then("a API deverá retornar o código do IBGE do endereço") do
-    #log "response body: #{$response.body}"
+    $cep = gets.chomp
     cepFromAPI = "#{$response['cep']}"
     if cepFromAPI == $cep
         log "Código IBGE do CEP buscado: #{$response['ibge']}"
@@ -17,7 +16,7 @@ Then("a API deverá retornar o código do IBGE do endereço") do
 end
 
 Then('deverá mostrar a mensagem que o CEP nao existe') do
-    #log "response body: #{$response.body}"
+    $cep = gets.chomp
     cepFromAPI = "#{$response['cep']}"
     if cepFromAPI != $cep
         log "O CEP buscado é inválido."
